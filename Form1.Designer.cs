76a2f8924cd09f2c690f53de79e2b5dc6388a532
uriyd <uriyd@MEGUAI>
2024-12-10 16:06:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Sem3Lab9Source2
+{
+    partial class Form1
+    {
+     
+        private System.ComponentModel.IContainer components = null;
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        public void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(30, 30, 30);
+            listBox1.ForeColor = Color.White;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(10, 10);
+            listBox1.Margin = new Padding(2);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(820, 344);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(45, 45, 45);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(86, 420);
+            textBox1.Margin = new Padding(2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(714, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(18, 420);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Вывод:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(18, 18, 18);
+            ClientSize = new Size(844, 488);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Погода";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        public ListBox listBox1;
+        public TextBox textBox1;
+        public Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Sem3Lab9Source2
+{
+    public partial class Form1 : Form
+    {
+        
+        private string File_Path = "C:/Users/uriyd/Downloads/city.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCityData();
+        }
+
+        // Метод для загрузки данных городов
+        private void LoadCityData()
+        {
+            string[] lines = File.ReadAllLines(File_Path);
+            foreach (string line in lines)
+            {
+                listBox1.Items.Add(line); // строку в listbox
+            }
+        }
+
+        // изменения выбора в listBox
+        private async void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем выбранную строку
+                string line = (string)listBox1.SelectedItem;
+                // Разделяем строку 
+                string[] parts = line.Split('\t');
+                string[] temp = parts[1].Split(',');
+
+                // координаты в тип double с учетом особенностей формата
+                if (temp.Length < 2)
+                {
+                    MessageBox.Show("Некорректный формат данных для координат.");
+                    return;
+                }
+
+                // Конвертируем строковые координаты в double
+                double latitude = ParseCoordinate(temp[0]);
+                double longitude = ParseCoordinate(temp[1]);
+
+                // Получаем текущую погоду по координатам
+                Weather currentWeather = await GetWeatherData(latitude, longitude);
+                DisplayInfo(currentWeather);
+            }
+            catch (Exception ex)
+            {
+                // Выводим сообщение об ошибке
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        // Метод отображения информации о погоде в текстовом поле
+        private void DisplayInfo(Weather weather)
+        {
+            textBox1.Text = $"Температура: {weather.Temp:F2}°C, Описание: {weather.Description}, Страна: {weather.Country}, Город: {weather.Name}";
+        }
+
+        // Метод для получения данных о погоде по широте и долготе
+        private static async Task<Weather> GetWeatherData(double latitude, double longitude)
+        {
+            string apiKey = "884603ba7ae4aa806e5952eafaea10f0";
+            string path = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}";
+            Weather weather;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Обрабатываем ответ сервера и парсим данные о погоде
+                    var dataString = await response.Content.ReadAsStringAsync();
+                    var data = JObject.Parse(dataString);
+                    weather = new Weather()
+                    {
+                        Country = (string)data["sys"]["country"],
+                        Name = (string)data["name"],
+                        Temp = Convert.ToDouble(data["main"]["temp"]) - 273.15, // Конвертация температуры из Кельвинов в Цельсии
+
+                        Description = (string)data["weather"][0]["description"]
+                    };
+                }
+                else
+                {
+                    throw new Exception("Не удалось получить данные о погоде.");
+                }
+            }
+            return weather;
+        }
+
+        // Метод для безопасного парсинга координат
+        private static double ParseCoordinate(string coordinate)
+        {
+            // Убираем пробелы и заменяем запятые на точки (если необходимо)
+            coordinate = coordinate.Trim().Replace(',', '.');
+            if (double.TryParse(coordinate, NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+            {
+                return result; // Возвращаем коориднату, если парсинг успешен
+            }
+            throw new Exception($"Некорректный формат координаты: {coordinate}");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Структура для хранения информации о погоде
+        public struct Weather
+        {
+            public string Country { get; set; }
+            public string Name { get; set; }
+            public double Temp { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
